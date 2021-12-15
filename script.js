cf6484f379f4892d3b15d6e9c5ffe776950a7964
Kendall Pinto <kendalldoescoding@gmail.com>
2021-12-16 00:28:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,8 +4,10 @@ function replaceText(element) {
     if (element.hasChildNodes()) {
         element.childNodes.forEach(replaceText)
     } else if (element.nodeType === Text.TEXT_NODE) {
-        if (element.textContent.match(/coronavirus/gi))
-        element.textContent = element.textContent.replace(/coronavirus/gi, '█████')
-        element.textContent = element.textContent.replace(/covid-19/gi, '█████')
+        if (element.textContent.match(/coronavirus/gi)) {
+        element.parentElement.style.color = "black"
+        element.parentElement.style.backgroundColor = "black"
+        }
+        element.textContent = element.textContent.replace(/coronavirus/gi, 'poopface')
     }
 }
